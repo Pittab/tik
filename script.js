a192d96286de27adb7ff012ab4a8d02030206634
Pittab <120305267+Pittab@users.noreply.github.com>
2024-04-26 11:45:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function tik(){
+const now = new Date();
+
+let hours = now.getHours();
+let minutes = now.getMinutes();
+
+hours = hours < 10 ? "0" + hours : hours;
+minutes = minutes < 10 ? "0" + minutes : minutes;
+
+const currentTime = hours + ":" + minutes;
+
+document.getElementById("tikklok").innerHTML=currentTime;
+}
+setInterval(tik, 1000);
