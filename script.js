53af6233b292b7510471f48d92df6db02d8ba4ac
Pittab <peter_rimmer@outlook.com>
2024-04-28 15:52:53 +0100

    added theming and some basic themes

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+function themes(theme){
+  if (theme == 'mj'){
+    document.body.style.color='#34be5b';
+    document.body.style.backgroundColor='#181a1b';
+  } else if (theme == 'mjlt'){
+    document.body.style.color='#34be5b';
+    document.body.style.backgroundColor='#fff';
+  } else if (theme == 'cpmab'){
+    document.body.style.backgroundColor='#24273a';
+    document.body.style.color='#8aadf4';
+  } else if (theme == 'od'){
+    document.body.style.backgroundColor='#282c34';
+    document.body.style.color='#abb2bf';
+  } else if (theme == 'lt'){
+    console.log("your punishment for light theme is this, change to dark theme now");
+    document.body.style.backgroundColor='#fff';
+    document.body.style.color='#fff';
+  } else{
+    console.log(theme+" is fucking non existent");
+  }
+}
+
 function tik(){
 const now = new Date();
 
